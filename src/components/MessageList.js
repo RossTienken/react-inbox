a7179d5a0884cd9159e484cce3c717b655bcd97e
Ross <ross.tienken99@gmail.com>
2018-01-10 10:34:30 -0700

    setting up api

---
+++ src/components/MessageList.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Message from './Message'
+
+const MessageList = ({
+  messages,
+  toggleSelect,
+  toggleStar
+}) => {
+  const messageComponents = messages.map(message => (
+    <Message
+      key = { message.id }
+      message={ message }
+      toggleSelect={ toggleSelect }
+      toggleStar={ toggleStar }
+    />
+  ))
+
+    return (
+      <div>
+        { messageComponents }
+      </div>
+    )
+}
+
+export default MessageList
